7a040f6c3a20ba46d0df91d94e467cbdd3efba96
ShanYouMuXi <2293902648@qq.com>
2024-08-17 17:33:46 +0800

    建立监测区域树与反馈建议

---
+++ cigarette-ui/src/views/cigarette/test/TreeNode.vue
@@ -0,0 +1,27 @@
+<template>
+    <li>
+      {{ node.name }}
+      <!-- 如果有子节点，则递归渲染子节点 -->
+      <ul v-if="isExpanded">
+        <tree-node
+          v-for="child in node.children"
+          :key="child.id"
+          :node="child"
+          :is-expanded="isExpanded(child)"
+        />
+      </ul>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TreeNode',
+    props: {
+      node: Object,
+      isExpanded: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+  </script>
