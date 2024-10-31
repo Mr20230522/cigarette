6e78a631fe8900b9fd9a915e2ab97afb3e6a34b0
dhs <974522912@qq.com>
2024-10-31 17:27:19 +0800

    1.用行为记录表和涉案表，统计车辆，人员出现次数涉案次数,涉案车辆信息页面的出现次数和涉案次数方法实现 2.完善前端已申报车辆和未申报车辆页面，已录入车辆就是在车辆信息表里面存在的车辆，反之就是未录入，并且前端的页面无法修改这些信息（表） 3.添加了一个表对未申报车辆信息的录入，当新增车辆信息每新增一个数据就会增同时增加这两个表，然后未录入车辆查显示的就是兴新增加的这个表的内容 4.数据库车辆信息表修改了一个列，涉案数，默认为一，1中四个数据也被我修改为bigint类型（未修改之前是varchar的）

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/cigarette/vehicle/TobVehicleController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/cigarette/vehicle/TobVehicleController.java
@@ -47,6 +47,18 @@ public class TobVehicleController extends BaseController
     }
 
     /**
+     * 查询新增车辆列表
+     */
+    @PreAuthorize("@ss.hasPermi('vehicle:vehicle:list')")
+    @GetMapping("/insertlist")
+    public TableDataInfo insertlist(TobVehicle tobVehicle)
+    {
+        startPage();
+        List<TobVehicle> list = tobVehicleService.selectTobVehicleInsertList(tobVehicle);
+        return getDataTable(list);
+    }
+
+    /**
      * 导出车辆列表
      */
     @PreAuthorize("@ss.hasPermi('vehicle:vehicle:export')")
