5e521df93346e16c2537f60184656044fdb88b8e
First-Use <xiaobai@qq.com>
2025-08-01 11:50:06 +0800

    优化后端嫌疑度计算                    20250801

--- cigarette-system/src/main/java/com/ruoyi/system/service/IToVehicleRealTimMonitoringService.java
+++ cigarette-system/src/main/java/com/ruoyi/system/service/IToVehicleRealTimMonitoringService.java
@@ -2,8 +2,6 @@ package com.ruoyi.system.service;
 
 import com.ruoyi.system.domain.ToVehicleRealTimMonitoring;
 
-import java.util.List;
-
 public interface IToVehicleRealTimMonitoringService {
     //    List<ToVehicleRealTimMonitoring> selectToVehicleRealTimMonitoringList(ToVehicleRealTimMonitoring query);
     ToVehicleRealTimMonitoring getNextRecord(Long lastId);
