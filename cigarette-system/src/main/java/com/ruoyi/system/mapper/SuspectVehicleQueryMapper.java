739c5cad67ad438260ced296799aaf14df97dd36
First-Use <xiaobai@qq.com>
2025-12-07 16:20:04 +0800

    嫌疑查询后端功能开发

--- cigarette-system/src/main/java/com/ruoyi/system/mapper/SuspectVehicleQueryMapper.java
+++ cigarette-system/src/main/java/com/ruoyi/system/mapper/SuspectVehicleQueryMapper.java
@@ -1,4 +1,12 @@
+
 package com.ruoyi.system.mapper;
 
-public class SuspectVehicleQueryMapper {
+import com.ruoyi.system.domain.ToVehicleRealTimMonitoring;
+import com.ruoyi.system.domain.vo.SuspectVehicleQueryVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SuspectVehicleQueryMapper {
+    List<ToVehicleRealTimMonitoring> selectSuspectVehicleList(@Param("query") SuspectVehicleQueryVO query);
 }
