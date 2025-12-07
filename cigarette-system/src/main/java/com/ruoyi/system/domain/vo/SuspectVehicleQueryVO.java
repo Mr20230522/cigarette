ef52f1a88c9ccce2377abc3f11ee4d7f872f876c
First-Use <xiaobai@qq.com>
2025-12-07 16:08:24 +0800

    嫌疑查询后端功能开发

---
+++ cigarette-system/src/main/java/com/ruoyi/system/domain/vo/SuspectVehicleQueryVO.java
@@ -0,0 +1,19 @@
+package com.ruoyi.system.domain.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 嫌疑车辆查询条件
+ */
+@Data
+public class SuspectVehicleQueryVO {
+    private String plate;           // 车牌号码（模糊）
+    private String plateType;       // 车牌类型
+    private String plateColor;      // 车牌颜色
+    private String vehicleType;     // 车辆类型
+    private String vehicleColor;    // 车身颜色
+    private Date startTime;         // 捕获开始时间
+    private Date endTime;           // 捕获结束时间
+}
