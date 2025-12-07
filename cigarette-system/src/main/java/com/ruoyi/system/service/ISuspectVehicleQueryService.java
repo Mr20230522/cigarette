af27508866e14adc763373915e00ab662427c1a2
First-Use <xiaobai@qq.com>
2025-12-07 16:20:14 +0800

    嫌疑查询后端功能开发

--- cigarette-system/src/main/java/com/ruoyi/system/service/ISuspectVehicleQueryService.java
+++ cigarette-system/src/main/java/com/ruoyi/system/service/ISuspectVehicleQueryService.java
@@ -1,4 +1,10 @@
 package com.ruoyi.system.service;
 
-public class ISuspectVehicleQueryService {
+import com.ruoyi.system.domain.ToVehicleRealTimMonitoring;
+import com.ruoyi.system.domain.vo.SuspectVehicleQueryVO;
+
+import java.util.List;
+
+public interface ISuspectVehicleQueryService {
+    List<ToVehicleRealTimMonitoring> selectSuspectVehicleList(SuspectVehicleQueryVO query);
 }
--- cigarette-system/src/main/java/com/ruoyi/system/service/impl/SuspectVehicleQueryServiceImpl.java
+++ cigarette-system/src/main/java/com/ruoyi/system/service/impl/SuspectVehicleQueryServiceImpl.java
@@ -1,4 +1,22 @@
 package com.ruoyi.system.service.impl;
 
-public class SuspectVehicleQueryServiceImpl {
+import com.ruoyi.system.domain.ToVehicleRealTimMonitoring;
+import com.ruoyi.system.domain.vo.SuspectVehicleQueryVO;
+import com.ruoyi.system.mapper.SuspectVehicleQueryMapper;
+import com.ruoyi.system.service.ISuspectVehicleQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SuspectVehicleQueryServiceImpl implements ISuspectVehicleQueryService {
+
+    @Autowired
+    private SuspectVehicleQueryMapper suspectVehicleQueryMapper;
+
+    @Override
+    public List<ToVehicleRealTimMonitoring> selectSuspectVehicleList(SuspectVehicleQueryVO query) {
+        return suspectVehicleQueryMapper.selectSuspectVehicleList(query);
+    }
 }
