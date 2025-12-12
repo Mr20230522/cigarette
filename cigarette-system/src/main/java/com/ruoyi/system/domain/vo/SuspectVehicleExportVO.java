e1e11df45a3f9a5bdb45269736354f787690c523
First-Use <xiaobai@qq.com>
2025-12-12 17:50:41 +0800

    嫌疑查询 导出查询的数据的VO类（Excel）。

---
+++ cigarette-system/src/main/java/com/ruoyi/system/domain/vo/SuspectVehicleExportVO.java
@@ -0,0 +1,52 @@
+package com.ruoyi.system.domain.vo;
+
+import com.ruoyi.common.annotation.Excel;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class SuspectVehicleExportVO {
+    /** 相机名称 */
+    @Excel(name = "相机名称")
+    private String cameraName;
+
+    /** 车道号 */
+    @Excel(name = "车道号")
+    private Integer laneNo;
+
+    /** 方向 */
+    @Excel(name = "方向")
+    private String direction;
+
+    /** 车速(km/h) */
+    @Excel(name = "车速")
+    private Integer speed;
+
+    /** 车牌 */
+    @Excel(name = "车牌")
+    private String plate;
+
+    /** 车牌类型 */
+    @Excel(name = "车牌类型")
+    private String plateType;
+
+    /** 车牌颜色 */
+    @Excel(name = "车牌颜色")
+    private String plateColor;
+
+    /** 车身颜色 */
+    @Excel(name = "车身颜色")
+    private String vehicleColor;
+
+    /** 车辆类型 */
+    @Excel(name = "车辆类型")
+    private String vehicleType;
+
+    /** 捕获时间 */
+    @Excel(name = "捕获时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss")
+    private Date captureTime;
+
+    /** 嫌疑程度 */
+    @Excel(name = "嫌疑程度")
+    private Integer level;
+}
