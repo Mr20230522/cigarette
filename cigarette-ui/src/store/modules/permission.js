dc45b18f153a12c25c7624d7f0934615627fcac5
First-Use <xiaobai@qq.com>
2026-01-03 15:01:23 +0800

    修复前端启动时的遇到的：Webpack 在构建 Vue 项目时，尝试处理一些它无法识别的文件类型 warning  in ./src/views/cigaretteFront/caution/uploadPersonnel.vue.bak warning  in ./src/views/cigaretteFront/dataVisualization copy.txt warning  in ./src/views/cigarette/test/test copy.txt warning  in ./src/views/cigaretteFront/dataVisualization/yndistricts.txt 上述四个文件属于不能识别的类型，通过修改自动导入逻辑，排除非.vue 文件，只匹配.vue文件。

--- cigarette-ui/src/store/modules/permission.js
+++ cigarette-ui/src/store/modules/permission.js
@@ -127,10 +127,10 @@ export function filterDynamicRoutes(routes) {
 
 export const loadView = (view) => {
   if (process.env.NODE_ENV === 'development') {
-    return (resolve) => require([`@/views/${view}`], resolve)
+    return (resolve) => require([`@/views/${view}.vue`], resolve)
   } else {
     // 使用 import 实现生产环境的路由懒加载
-    return () => import(`@/views/${view}`)
+    return () => import(`@/views/${view}.vue`)
   }
 }
 
