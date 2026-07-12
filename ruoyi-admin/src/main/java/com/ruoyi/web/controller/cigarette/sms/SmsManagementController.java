2bafa064a4609eac726f9a041f5a3ac2179fedec
Aerlany <1243535201@qq.com>
2026-07-12 14:11:30 +0800

    测试提交

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/cigarette/sms/SmsManagementController.java
@@ -0,0 +1,7 @@
+package com.ruoyi.web.controller.cigarette.sms;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/message")
+public class SmsManagementController {
+}
